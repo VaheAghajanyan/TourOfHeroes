7846bab4c4b14fddd5a537127c64dd64443d9c77
Vahe <vah.aghajanyan@gmail.com>
2022-01-18 17:41:24 +0400

    Displaying list of heroes

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {Hero} from "./hero";
+import {HEROES} from "../mock-heroes";
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = HEROES;
+  selectedHero?: Hero;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    if (this.selectedHero) {
+      this.selectedHero = undefined;
+    } else {
+      this.selectedHero = hero;
+    }
+  }
+}
